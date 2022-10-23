aeeafccc6d08b1157200921a14d371be3bac0d06
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-23 09:31:19 +0100

    stock solution

--- Easy/best time to buy and sell stock.js
+++ Easy/best time to buy and sell stock.js
@@ -24,20 +24,39 @@
  *Explanation: In this case, no transactions are done and the max profit = 0.
 */
 
+//Solution
 
+var maxProfit = function (prices) {
+  let left = 0 // Buying price
+  let right = 1 // Selling price
+  let maxProfit = 0
+  while (right < prices.length) {
+    if (prices[right] > prices[left]) {
+      let profit =
+        prices[right] - prices[left]
+      if (profit > maxProfit) {
+        maxProfit = profit
+      }
+    } else {
+      left = right
+    }
+    right += 1
+  }
+  return maxProfit
+}
 
 //Test Cases
 //Copy the below to a test file having a .test.js extension and run `npm run test` in your terminal.
 
 describe('Tests', () => {
-	it('test', () => {
-		Test.assertEquals(
-			maxProfit([7,1,5,3,6,4]),
-			5,
-		)
-		Test.assertEquals(
-			maxProfit([7,6,4,3,1]),
-			0,
-		)
-	})
+  it('test', () => {
+    Test.assertEquals(
+      maxProfit([7, 1, 5, 3, 6, 4]),
+      5
+    )
+    Test.assertEquals(
+      maxProfit([7, 6, 4, 3, 1]),
+      0
+    )
+  })
 })
