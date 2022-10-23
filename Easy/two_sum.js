0b92827b68a3618f2f1638b3ab78e899457182ea
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-23 09:32:32 +0100

    two-sum

--- Easy/two_sum.js
+++ Easy/two_sum.js
@@ -1,10 +1,10 @@
 /**
  * Description:
- * 
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * 
+ *
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
@@ -16,35 +16,42 @@
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  */
 
-
 // Solution
 
 var twoSum = function (nums, target) {
-	for (let i = 0; i < nums.length; i++) {
-		for (
-			let k = i + 1;
-			k < nums.length;
-			k++
-		) {
-			if (nums[i] + nums[k] == target) {
-				return [i, k]
-			}
-		}
-	}
+  for (
+    let i = 0;
+    i < nums.length;
+    i++
+  ) {
+    for (
+      let k = i + 1;
+      k < nums.length;
+      k++
+    ) {
+      if (nums[i] + nums[k] == target) {
+        return [i, k]
+      }
+    }
+  }
 }
 
 // More suitable and faster solution
 
 var twoSum2 = function (nums, target) {
-	prevMap = {}
+  prevMap = {}
 
-	for (let i = 0; i < nums.length; i++) {
-		diff = target - nums[i]
-		if  (diff in prevMap) {
-			return [prevMap[diff], i]
-		}
-		prevMap[nums[i]] = i
-	}
+  for (
+    let i = 0;
+    i < nums.length;
+    i++
+  ) {
+    diff = target - nums[i]
+    if (diff in prevMap) {
+      return [prevMap[diff], i]
+    }
+    prevMap[nums[i]] = i
+  }
 }
 
 console.log(twoSum2([3, 2, 4], 6))
@@ -52,14 +59,14 @@ console.log(twoSum2([3, 2, 4], 6))
 //Copy the below to a test file having a .test.js extension and run `npm run test` in your terminal.
 
 describe('Tests', () => {
-	it('test', () => {
-		Test.assertEquals(
-			twoSum([2, 7, 11, 15], 9),
-			[0, 1],
-		)
-		Test.assertEquals(
-			twoSum([3, 2, 4], 6),
-			[1, 2],
-		)
-	})
+  it('test', () => {
+    Test.assertEquals(
+      twoSum([2, 7, 11, 15], 9),
+      [0, 1]
+    )
+    Test.assertEquals(
+      twoSum([3, 2, 4], 6),
+      [1, 2]
+    )
+  })
 })
